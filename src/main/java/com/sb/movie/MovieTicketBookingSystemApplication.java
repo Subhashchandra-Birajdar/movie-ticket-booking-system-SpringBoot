8b73de125a831e0856a3ec04087948c4e1cd8e96
subhash birajdar <subhashchandrabirajdar98@gmail.com>
2024-08-27 11:09:15 +0530

    Added Repositories

--- src/main/java/com/sb/movie/MovieTicketBookingSystemApplication.java
+++ src/main/java/com/sb/movie/MovieTicketBookingSystemApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MovieTicketBookingSystemApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MovieTicketBookingSystemApplication.class, args);
 	}
 
